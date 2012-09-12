8a91b747ee31b8d1cccce7707d500c8590b3dcb9
Leon Williams <leonsw@gmail.com>
2012-09-12 13:38:55 -0400

    First Commit

---
+++ perfect_evening/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PerfectEvening::Application.config.secret_token = '5b179c60e204eb59f69927f588818e8e7bada45a9d8f3a57bd7f8e05f758e6b9bbba085c665086fdad7496d362234bc767a85efd92c80bf64f01f2fc451047fd'
---
+++ perfect_evening/config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+PerfectEvening::Application.config.session_store :cookie_store, key: '_perfect_evening_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# PerfectEvening::Application.config.session_store :active_record_store
